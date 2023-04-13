060bc089f107f34db20cdee364e59a0fd5438546
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2023-04-13 06:08:42 +0000

    :guitar: pattern send notification

---
+++ notification.php
@@ -0,0 +1,13 @@
+<?php
+  $webhook_url = 'ลิงค์นี้ ไปก็อบจาก ตั้งค่าแชท chanal แล้วไปก็อบ webhook มา';
+
+  $message = array('content' => 'Hello, world!');
+
+  $curl = curl_init($webhook_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($curl, CURLOPT_POST, 1);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($message));
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_exec($curl);
+  curl_close($curl);
+?>
