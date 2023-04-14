5d387784606316dc46599b636394e17613f724aa
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2023-04-14 18:10:13 +0700

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,14 +1,18 @@
 <?php
-  $webhook_url = 'ลิงค์นี้ ไปก็อบจาก ตั้งค่าแชท chanal แล้วไปก็อบ webhook มา';
-  $commit = isset($_GET["commit"]) ? $_GET["commit"] : '';
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
-  $message = array('content' => 'Deploy success by guitar! \n commit : '.$commit);
+$webhook_url = 'ใส่ url webhook';
 
-  $curl = curl_init($webhook_url);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-  curl_setopt($curl, CURLOPT_POST, 1);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($message));
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_exec($curl);
-  curl_close($curl);
+$requestBody = json_decode(file_get_contents('php://input'), true);
+$message = array('content' => '[BACKEND] Deploy Success!!! :guitar:'.$requestBody["commit"]);
+
+$curl = curl_init($webhook_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($message));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_exec($curl);
+curl_close($curl);
 ?>
